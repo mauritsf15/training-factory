d8be78a1b656719c2dfc61b0acb2df3449493ab1
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-06-14 13:37:46 +0200

    Updates

--- src/Repository/EnrollmentsRepository.php
+++ src/Repository/EnrollmentsRepository.php
@@ -21,7 +21,7 @@ class EnrollmentsRepository extends ServiceEntityRepository
         parent::__construct($registry, Enrollments::class);
     }
 
-    public function save(Enrollments $entity, bool $flush = false): void
+    public function save(Enrollments $entity, bool $flush = true): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class EnrollmentsRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Enrollments $entity, bool $flush = false): void
+    public function remove(Enrollments $entity, bool $flush = true): void
     {
         $this->getEntityManager()->remove($entity);
 
